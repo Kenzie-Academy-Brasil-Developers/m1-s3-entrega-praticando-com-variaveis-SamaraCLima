6afa7d6e239a0f3db1578972cad0771ce35589bf
SamaraCLima <103124423+SamaraCLima@users.noreply.github.com>
2022-06-13 10:41:38 -0300

    s3-11

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Jhon"
+let sobrenome = "Doyle"
+let anoAtual = 2022;
+let anoNascimento = 1991;
+let idade = anoAtual - anoNascimento;
+let cidade = "Curitiba"
+
+console.log(`Eu sou o ${nome} e tenho ${idade} anos e moro em ${cidade}`)
